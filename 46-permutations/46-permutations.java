4e644bd43ed2801cfd6ecb1799ef42bd4194ebaa
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-01-08 13:07:29 +0530

    Time: 2 ms (55.95%), Space: 39.4 MB (48.99%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -3,19 +3,19 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> asf = new ArrayList<>();
         Set<Integer> set = new HashSet<>();
-        solution(nums,ans,asf,set,nums.length);
+        solution(nums,ans,asf,set,0,nums.length);
         return ans;
     }
-    void solution(int[] nums, List<List<Integer>> ans, List<Integer> asf, Set<Integer> set, int n){
+    void solution(int[] nums, List<List<Integer>> ans, List<Integer> asf, Set<Integer> set, int idx, int n){
       if(set.size()==n){
           ans.add(new ArrayList<>(asf));
           return;
       }  
-      for(int i=0;i<n;i++){
+      for(int i=idx;i<n;i++){
           if(!set.contains(nums[i])){
             set.add(nums[i]);
               asf.add(nums[i]);
-              solution(nums,ans,asf,set,n);
+              solution(nums,ans,asf,set,idx,n);
               set.remove(nums[i]);
               asf.remove(asf.size()-1);
           }
