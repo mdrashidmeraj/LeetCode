d48ff3be692cfb4f4cc8b505a71c6bb86d62406a
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-02-08 21:42:03 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (18.71%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,16 +1,23 @@
 class Solution {
     public int addDigits(int num) {
-        int sum = 0;
-        while(num>9){
-            sum =0;
-            while(num>0){
-                int d = num%10;
-                sum+=d;
-                num/=10;
-            }
-            num = sum;
-        }
-        return num;
+//         My approach working
+        // int sum = 0;
+        // while(num>9){
+        //     sum =0;
+        //     while(num>0){
+        //         int d = num%10;
+        //         sum+=d;
+        //         num/=10;
+        //     }
+        //     num = sum;
+        // }
+        // return num;
+
+        //o(1) approach
+        
+        if(num == 0) return 0;
+        if(num%9 == 0) return 9;
+        return num%9;
         
     }
 }
