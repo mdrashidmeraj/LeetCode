24ec807d853019faa1f58cb6d458d995b7bc79d8
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-02-09 14:47:33 +0530

    Create Grid Unique Paths - interviewBit.java

---
+++ Grid Unique Paths - interviewBit.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int uniquePaths(int A, int B) {
+        int n = A+B-2;
+        int r = A-1;
+        double res = 1;
+        for(int i =1;i<=r;i++){
+            res = res*(n-r+i)/i;
+        }
+        return (int)(res);
+    }
+}
