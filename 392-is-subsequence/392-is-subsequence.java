792014bbc207f84a7f3de21ff399bdc42de94be8
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-03-02 18:40:04 +0530

    Time: 9 ms (6.47%), Space: 42.3 MB (15.23%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j =0;j<=m;j++){
+                if(i==0 || j==0) dp[i][j] = 0;
+                else{
+                    if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
+                    else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        // System.out.println(dp[n][m]);
+        return dp[n][m] == n;
+    }
+}
