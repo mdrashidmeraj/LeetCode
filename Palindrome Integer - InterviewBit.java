57fc6dad0d2b99aa5d6374279e62adcffa140a58
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-03-02 18:48:13 +0530

    Create Palindrome Integer - InterviewBit.java

---
+++ Palindrome Integer - InterviewBit.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int isPalindrome(int A) {
+        String s = A+"";
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)) return 0;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+}
