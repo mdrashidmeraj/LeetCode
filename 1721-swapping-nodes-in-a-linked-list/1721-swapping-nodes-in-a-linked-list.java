7d6c285b3c7639bef39e9ec0f0973d41f31b382a
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-04-04 10:51:24 +0530

    Time: 2 ms (100.00%), Space: 57 MB (87.82%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,25 +10,33 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        int len = 0;
-        ListNode temp = head;
-        while(temp != null){
-            temp = temp.next;
-            len++;
+        // int len = 0;
+        // ListNode temp = head;
+        // while(temp != null){
+        //     temp = temp.next;
+        //     len++;
+        // }
+        // temp = head;
+        // int t = k-1;
+        // while(temp != null && t != 0){
+        //     temp = temp.next;
+        //     t--;
+        // }
+        // ListNode temp2 = head;
+        // t = len-k;
+        // while(temp2 != null && t != 0){
+        //     temp2 = temp2.next;
+        //     t--;
+        // }
+        ListNode temp = null;
+        ListNode temp2 = null;
+        for(ListNode t = head; t != null; t = t.next){
+            temp2 = temp2 == null ? null : temp2.next;
+            if(--k == 0){
+                temp = t;
+                temp2 = head;
+            }
         }
-        temp = head;
-        int t = k-1;
-        while(temp != null && t != 0){
-            temp = temp.next;
-            t--;
-        }
-        ListNode temp2 = head;
-        t = len-k;
-        while(temp2 != null && t != 0){
-            temp2 = temp2.next;
-            t--;
-        }
-        
         int val = temp.val;
         temp.val = temp2.val;
         temp2.val = val;
