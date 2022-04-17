d786a4863b4fa46d971c66a274d2ec746c38f439
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-04-17 22:50:57 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (88.54%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        TreeNode head = null, pre = null;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            if (head == null) {
+                head = cur;
+            }
+            cur.left = null;
+            if (pre != null) {
+                pre.right = cur;
+            }
+            pre = cur;
+            cur = cur.right;
+        }
+        return head;
+    }
+}
