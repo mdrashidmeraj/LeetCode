4ec6000c89dcb330926c50f0e92f1a36312ffe80
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-08-05 02:22:16 +0530

    Time: 24 ms (92.70%), Space: 43.5 MB (85.50%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,9 +1,18 @@
 class MyCalendar {
-    private List<int[]> books = new ArrayList<>();
+
+    TreeMap<Integer, Integer> calendar;
+
+    public MyCalendar() {
+        calendar = new TreeMap<>();
+    }
+
     public boolean book(int start, int end) {
-        for (int[] b : books)
-            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-        books.add(new int[]{ start, end });
+        Integer floorKey = calendar.floorKey(start);
+        if (floorKey != null && calendar.get(floorKey) > start) return false;
+        Integer ceilingKey = calendar.ceilingKey(start);
+        if (ceilingKey != null && ceilingKey < end) return false;
+
+        calendar.put(start, end);
         return true;
     }
 }
