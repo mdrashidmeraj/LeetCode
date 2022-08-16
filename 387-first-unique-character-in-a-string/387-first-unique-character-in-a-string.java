68f2fe1426472e42b2dbc697dbb99bb5f8c0ae85
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-08-17 01:26:55 +0530

    Time: 24 ms (65.83%), Space: 47.8 MB (83.94%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,24 +1,32 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
+        // HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
+        int ans = Integer.MAX_VALUE;
+        // for(int i = 0; i<s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     if(map.containsKey(ch)) {
+        //         ArrayList<Integer> a = map.get(ch);
+        //         a.add(i);
+        //         map.put(ch, a);    
+        //     }else{
+        //         ArrayList<Integer> arr = new ArrayList<>();
+        //         arr.add(i);
+        //         map.put(ch, arr);   
+        //     }
+        // }
+        // for(char ch: map.keySet()){
+        //     if(map.get(ch).size() == 1){
+        //         ans = Math.min(ans, map.get(ch).get(0));
+        //     } 
+        // }
         for(int i = 0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(map.containsKey(ch)) {
-                ArrayList<Integer> a = map.get(ch);
-                a.add(i);
-                map.put(ch, a);    
-            }else{
-                ArrayList<Integer> arr = new ArrayList<>();
-                arr.add(i);
-                map.put(ch, arr);   
-            }
+            int idx = s.indexOf(ch);
+            int idx2 = s.lastIndexOf(ch);
+            if(idx == idx2) return i;
         }
-        int ans = Integer.MAX_VALUE;
-        for(char ch: map.keySet()){
-            if(map.get(ch).size() == 1){
-                ans = Math.min(ans, map.get(ch).get(0));
-            } 
-        }
-        return ans == Integer.MAX_VALUE ?  -1:  ans;
+        return -1;
+
+        // return ans == Integer.MAX_VALUE ?  -1:  ans;
     }
 }
