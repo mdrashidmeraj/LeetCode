53df14a72fc444a3406862d6e4eb0726f404b06e
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-08-18 17:51:31 +0530

    Time: 1 ms (92.91%), Space: 41.7 MB (78.65%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -2,17 +2,30 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j =0;j<=m;j++){
-                if(i==0 || j==0) dp[i][j] = 0;
-                else{
-                    if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
-                    else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
+        //         DP APPROACH
+        // int[][] dp = new int[n+1][m+1];
+        // for(int i=0;i<=n;i++){
+        //     for(int j =0;j<=m;j++){
+        //         if(i==0 || j==0) dp[i][j] = 0;
+        //         else{
+        //             if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
+        //             else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        //         }
+        //     }
+        // }
+        // return dp[n][m] == n;
+//         
+        if(n == 0) return true;
+        if(n == 0 && m == 0) return true;
+        int i = 0;
+        int j= 0;
+        while(j<m){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
             }
+            if(i == s.length()) return true;
+            j++;
         }
-        // System.out.println(dp[n][m]);
-        return dp[n][m] == n;
+        return false;
     }
 }
