d201ab3586c7699681d2b522991ba7c930763142
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-09-06 14:42:20 +0530

    Time: 32 ms (12.48%), Space: 50.3 MB (67.89%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b)-> a.getValue()-b.getValue());
+        for(Map.Entry<Integer, Integer> e: map.entrySet()){
+            pq.add(e);
+            if(pq.size()>k) pq.remove();
+        }
         int[] ans = new int[k];
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int n: nums){
-            map.put(n, map.getOrDefault(n,0)+1);
-        }
-           
-        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = 
-                         new PriorityQueue<>((a,b)->(b.getValue()-a.getValue()));
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            maxHeap.add(entry);
-        }
-        
-        List<Integer> res = new ArrayList<>();
-        while(res.size()<k){
-            Map.Entry<Integer, Integer> entry = maxHeap.poll();
-            res.add(entry.getKey());
-        }
-        int t = 0;
-        for(int i : res){
-            ans[t++] = i;
+        for(int i = k-1; i>=0; i--){
+            ans[i] = pq.remove().getKey();
         }
         return ans;
     }
