9327c6f1635f8f3bc0d4c75ab8464a38f22b3d9b
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-10-04 02:58:10 +0530

    Time: 4 ms (70.00%), Space: 44.3 MB (59.17%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,21 +16,18 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return helper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
-        
     }
-    TreeNode helper(int[] pre, int sp, int ep, int[] in, int si, int ei){
+    
+    TreeNode helper(int[] preorder, int sp, int ep, int[] inorder, int si, int ei){
         if(sp>ep || si>ei) return null;
-        
-        
-        int i = si;
-        for(;i<in.length;i++){
-            if(in[i] == pre[sp]) break;
+        int i = 0;
+        for(;i<inorder.length;i++){
+            if(inorder[i] == preorder[sp]) break;
         }
-        TreeNode root = new TreeNode(pre[sp]);
-        int countOfLeftElement = i-si;
-         root.left = helper(pre,sp+1,sp+countOfLeftElement, in, si, i-1); 
-         root.right = helper(pre, sp+countOfLeftElement+1, ep, in, i+1, ei);
-        return root;
         
+        TreeNode root = new TreeNode(preorder[sp]);
+        root.left = helper(preorder,sp+1 ,sp+i-si, inorder, si, i-1);
+        root.right = helper(preorder,sp+i-si+1 , ep , inorder, i+1, ei);
+        return root;
     }
 }
