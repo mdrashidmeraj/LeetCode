5f507b6c90c10455990c7395c23209722178634b
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-10-09 15:12:08 +0530

    Time: 411 ms (36.69%), Space: 185.8 MB (82.60%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,28 +1,38 @@
-public class TimeMap {
-
-    private Map<String,TreeMap<Integer,String>> map;
-
-    /** Initialize your data structure here. */
+class TimeMap {
+    HashMap<String, TreeMap<Integer, String>> map; 
     public TimeMap() {
         map = new HashMap<>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        if(!map.containsKey(key)) {
-            map.put(key,new TreeMap<>());
+        if(map.containsKey(key)){
+            TreeMap<Integer, String> tm = map.get(key);
+            tm.put(timestamp, value);
+        }else{
+            map.put(key, new TreeMap<Integer, String>());
+            TreeMap<Integer, String> tm = map.get(key);
+            tm.put(timestamp, value);
         }
-        map.get(key).put(timestamp,value);
     }
-
+    
     public String get(String key, int timestamp) {
-        TreeMap<Integer,String> treeMap = map.get(key);
-        if(treeMap==null) {
-            return "";
+        if(map.containsKey(key)){
+            TreeMap<Integer, String> tm = map.get(key);
+            while(timestamp > 0){
+                if(tm.containsKey(timestamp)){
+                    return tm.get(timestamp);
+                }
+                timestamp--;
+            }
+
         }
-        Integer floor = treeMap.floorKey(timestamp);
-        if(floor==null) {
-            return "";
-        }
-        return treeMap.get(floor);
+        return "";
     }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
