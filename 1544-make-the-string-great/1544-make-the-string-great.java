7b956a386f959fbf6e718cd88f497cc553d44b58
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2022-11-08 09:27:19 +0530

    Time: 8 ms (26.21%), Space: 43.1 MB (34.52%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,28 +1,15 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> st = new Stack<>();
-        for(char c: s.toCharArray()){
-            if(st.size() == 0) st.push(c);
-            else{
-                char ch = st.peek();
-                if(c>='A' && c<='Z' && ch>='a' && ch<='z'){
-                    if((ch+"").equals((c+"").toLowerCase())){
-                        st.pop();
-                        continue;
-                    }
-                }else if(c>='a' && c<='z' && ch>='A' && ch<='Z'){
-                    if((c+"").equals((ch+"").toLowerCase())){
-                        st.pop();
-                        continue;
-                    }
-                    
-                }
-                st.push(c);
-            }
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(!stack.isEmpty() && Math.abs(stack.peek()-s.charAt(i)) == 32)
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
         }
         StringBuilder sb = new StringBuilder();
-        while(st.size()>0){
-            sb.insert(0,st.pop());
+        while(stack.size()>0){
+            sb.insert(0,stack.pop());
         }
         return sb.toString();
     }
